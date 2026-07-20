e678e734e44f4387bb3e76934f01d536db657a62
Soham <56361650+Kaizenick@users.noreply.github.com>
2026-07-19 22:04:54 -0400

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String cut = "#-#-#-#-#-#";
+        String encoded = "";
+        if(strs.size() == 0){
+            return "";
+        }
+        for(int i = 0; i< strs.size(); i++){
+            if(strs.get(i)==""){
+                encoded += "emptyString";
+            }
+            encoded += strs.get(i);
+            if(i==strs.size() - 1){
+                break;
+            }
+            encoded+=cut;
+        }
+        System.out.println(encoded);
+        return encoded;
+    }
+
+    public List<String> decode(String str) {
+        if(str==""){
+            return new ArrayList<String>();
+        }
+        ArrayList<String> ans = new ArrayList<>((Arrays.stream(str.split("#-#-#-#-#-#")).toList()));
+        for(int i = 0; i<ans.size(); i++){
+            if(ans.get(i).equals("emptyString")){
+                ans.set(i,"");
+            }
+        }
+        System.out.println(ans);
+        return ans;
+    }
+}
